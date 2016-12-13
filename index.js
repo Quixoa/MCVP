0e3a8d997d16781332fc4a924621d686cc9e6a02
AJHacker <arihant@quixoa.com>
2016-12-13 17:08:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('Hello World!');
 })
-app.listen(80, function() {
+app.listen(process.env.PORT, function() {
   console.log("Node app is running");
 })
