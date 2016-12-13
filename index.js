292138514423ca37f113c3bf757671e0761fa0b3
AJHacker <arihant@quixoa.com>
2016-12-13 17:26:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-// var express = require('express');
-// var io = require('socket.io');
-// var app = express();
+var express = require('express');
+var io = require('socket.io');
+var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 
-// app.get('/', function(request, response) {
-//   response.send("HI");
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(request, response) {
+  response.send("HI");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
