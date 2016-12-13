697876ad8d1c4183856518e38b06332493b23897
AJHacker <arihant@quixoa.com>
2016-12-13 17:19:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var io = require('socket.io');
 var app = express();
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-})
-app.listen(process.env.PORT, function() {
-  console.log("Node app is running");
-})
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(request, response) {
+  response.send("HI");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
