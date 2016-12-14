bc81c1173685fa43e8e0838acccade9475eb10d5
AJHacker <arihant@quixoa.com>
2016-12-13 19:14:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ app.set('port', (process.env.PORT || 5000));
 
 
 app.get('/', function(request, response) {
-  response.send("HI");
+  response.send("<h1>Main View go to /host<h1>");
+  response.send("<h1>Mobile devices go to /player<h1>");
+
+});
+
+app.get('/host', function(request, response) {
+  response.send("<h1>HOST DEVICE<h1>");
+});
+
+app.get('/player', function(request, response) {
+  response.send("<h1>PLAYER<h1>");
 });
 
 app.listen(app.get('port'), function() {
