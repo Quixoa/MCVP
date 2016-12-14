ba3fdabea2de04987b3fa464338993f54b8cab60
AJHacker <arihant@quixoa.com>
2016-12-13 23:54:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-var express = require('express');
-var io = require('socket.io');
-var app = express();
+var app = require('express').createServer();
+var io = require('socket.io')(app);
 
-app.set('port', (process.env.PORT || 5000));
+app.listen(80);
 
-
-app.get('/', function(request, response) {
-  response.send("<h1>Main View go to /host<br>Device go to /player<h1>");
-
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/host', function(request, response) {
-  response.send("<h1>HOST DEVICE<h1>");
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
-app.get('/player', function(request, response) {
-  response.send("<h1>PLAYER<h1>");
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// var express = require('express');
+// var io = require('socket.io');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+
+// app.get('/', function(request, response) {
+//   response.send("<h1>Main View go to /host<br>Device go to /player<h1>");
+
+// });
+
+// app.get('/host', function(request, response) {
+//   response.send("<h1>HOST DEVICE<h1>");
+// });
+
+// app.get('/player', function(request, response) {
+//   response.send("<h1>PLAYER<h1>");
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
