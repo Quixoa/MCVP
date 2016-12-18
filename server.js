1493b20a41ed0cd8a43b9598e21e168f66f6975c
AJHacker <arihant@quixoa.com>
2016-12-18 17:27:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-'use strict';
+var express = require('express');
+var app = express();
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', function(request,response){
+  response.write('Hello World');
+  response.end();
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+app.listen(3000);
+
+
+
+
+
+// 'use strict';
+
+// const express = require('express');
+// const socketIO = require('socket.io');
+// const path = require('path');
+
+// const PORT = process.env.PORT || 3000;
+// const INDEX = path.join(__dirname, 'index.html');
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// const io = socketIO(server);
+
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
