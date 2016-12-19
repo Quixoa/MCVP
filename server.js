d88ff2ddd6fac705237d808f40b45d136607d133
AJHacker <arihant@quixoa.com>
2016-12-18 23:11:35 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ var server = express()
   .get('/',(req, res) => res.sendFile(INDEX))
   .get('/client',(req, res) => res.sendFile(CLIENTPATH))
   .get('/host',(req, res) => res.sendFile(HOSTPATH))
+  .use("/styles",express.static(__dirname + "/styles"));
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
