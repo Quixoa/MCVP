923412256218d8a3eef920f47427d93123b14dbd
AJHacker <arihant@quixoa.com>
2016-12-18 19:24:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var path = require('path');
 var PORT = process.env.PORT || 3000;
 var INDEX = path.join(__dirname, 'index.html');
 
-var server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var server = express();
+server.get('/', function(req,res){
+  res.sendFile(INDEX);
+}
+server.listen(PORT);
 
 var io = socketIO(server);
 var users = 0;
