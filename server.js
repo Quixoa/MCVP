7503a213775d06b59a490aafb7373077faeff3a6
AJHacker <arihant@quixoa.com>
2016-12-18 22:57:39 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,24 +6,55 @@ var path = require('path');
 
 var PORT = process.env.PORT || 3000;
 var INDEX = path.join(__dirname, 'index.html');
+var CLIENTPATH = path.join(__dirname, 'client.html');
+var HOSTPATH = path.join(__dirname, 'host.html');
+
+var data =
+{
+  'clients': 0,
+  'host': false,
+  'hostIDs':[],
+  'totalConnections':0,
+  'clientIDs':[],
+  'test': function() {
+    console.log('test');
+  }
+
+};
+
 
 var server = express()
-  .get('/',(req, res) => res.sendFile(INDEX) )
-  .get('/hello',(req, res) => res.send('Hello World') )
-  .get('/main',(req, res) => res.send('Main Connected') )
+  .get('/',(req, res) => res.sendFile(INDEX))
+  .get('/client',(req, res) => res.sendFile(CLIENTPATH))
+  .get('/host',(req, res) => res.sendFile(HOSTPATH))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
- 
-  
-  
+
+
+
 var io = socketIO(server);
-var users = 0;
+
+io.on('clientConnected',function(id){
+  data.clientIDs.push(id);
+  data.clients = data.clients + 1;
+
+});
+
+io.on('hostConnected',function(id){
+  data.hostIDs.push(id);
+  data.host = true;
+
+});
+
 io.on('connection', (socket) => {
-  users = users+1;
-  io.emit('con',users);
+  data.totalConnections = data.totalConnections+1;
   socket.on('disconnect',function(){
-    users = users-1
+    if(data.clientIDs.includes(socket.id)){
+        var index = data.clientIDs.indexOf(socket.id);
+        array.splice(index, 1);
+    }
+    data.totalConnections = data.totalConnections-1
   });
 });
 
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('tick', data, 1000);
